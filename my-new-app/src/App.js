d1d8689cad7cb1261cf37ba83a46c3890b79b8dc
Farhia Addow <farhia.dahir.addow@@gmaile.no>
2024-09-27 23:53:32 +0200

    react set up finised

---
+++ my-new-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Product() {
+  return <div>Milk: 19.99</div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+
+export default App;
